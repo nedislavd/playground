accd650979dfeb06dd4d344a3e1cae4239ff18cb
nedislavd <nedislav.denev@gmail.com>
2022-12-01 23:28:15 +0200

    AoC 2022 Day 1 Quick & Dirty solution

--- advent-of-code/src/day01/index.ts
+++ advent-of-code/src/day01/index.ts
@@ -3,15 +3,42 @@ import run from "aocrunner";
 const parseInput = (rawInput: string) => rawInput;
 
 const part1 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+  const listOfCalories = parseInput(rawInput);
+  const listOfCaloriesPerElf: string[] = listOfCalories.split("\n");
+  let sumOfCaloriesPerElf: number[] = [];
+  let sumPerElf: number = 0;
 
-  return;
+  for (let i = 0; i < listOfCaloriesPerElf.length; i++) {
+    if (listOfCaloriesPerElf[i] !== "") {
+      sumPerElf += parseInt(listOfCaloriesPerElf[i]);
+      continue;
+    }
+    sumOfCaloriesPerElf.push(sumPerElf);
+    sumPerElf = 0;
+  }
+  return Math.max(...sumOfCaloriesPerElf);
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+  const listOfCalories = parseInput(rawInput);
+  const listOfCaloriesPerElf: string[] = listOfCalories.split("\n");
+  let sumOfCaloriesPerElf: number[] = [];
+  let sumPerElf: number = 0;
 
-  return;
+  for (let i = 0; i < listOfCaloriesPerElf.length; i++) {
+    if (listOfCaloriesPerElf[i] !== "") {
+      sumPerElf += parseInt(listOfCaloriesPerElf[i]);
+      continue;
+    }
+    sumOfCaloriesPerElf.push(sumPerElf);
+    sumPerElf = 0;
+  }
+  return sumOfCaloriesPerElf
+    .sort(function (a, b) {
+      return b - a;
+    })
+    .slice(0, 3)
+    .reduce((sum, x) => sum + x);
 };
 
 run({
