5920e29290265348baa28909dfa962b65caf5fc7
nedislavd <nedislav.denev@gmail.com>
2023-12-03 18:04:16 +0200

    AoC 2023 Day 3 init

--- advent-of-code/2023/src/day03/index.ts
+++ advent-of-code/2023/src/day03/index.ts
@@ -5,7 +5,7 @@ const parseInput = (rawInput: string) => rawInput;
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  return input.trim().match(/(\d*(?<=[^\d.\n\r].{140,142})\d+)|(\d+(?=.{140,142}[^\d.\n\r])\d*)|((?<=[^\d.\n\r])\d+)|(\d+(?=[^\d.\n\r]))/gs)?.reduce((p,c) => p+ +c, 0);;
 };
 
 const part2 = (rawInput: string) => {
@@ -17,19 +17,28 @@ const part2 = (rawInput: string) => {
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `467..114..
+                ...*......
+                ..35..633.
+                ......#...
+                617*......
+                .....+.58.
+                ..592.....
+                ......755.
+                ...$.*....
+                .664.598..`,
+        expected: 4361,
+      },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: ``,
+        expected: "",
+      },
     ],
     solution: part2,
   },
