eed15a7097692c884f562612457fb838ecf36b1e
nedislavd <nedislav.denev@gmail.com>
2023-12-04 13:29:41 +0200

    AoC 2023 Day 3 done

--- advent-of-code/2023/README.md
+++ advent-of-code/2023/README.md
@@ -11,9 +11,9 @@
 
 <!--SOLUTIONS-->
 
-[![Day](https://badgen.net/badge/01/%E2%98%86%E2%98%86/green)](src/day01)
-[![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/green)](src/day02)
-![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
+![Day](https://badgen.net/badge/01/%E2%98%86%E2%98%86/gray)
+![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
@@ -83,16 +83,16 @@ Both parts: -
 
 ```
 Day 03
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 12.716ms
+Time part 2: 11.072ms
+Both parts: 23.788ms
 ```
 
 ```
 Day 04
-Time part 1: 4.097ms
-Time part 2: 195.366ms
-Both parts: 199.463ms
+Time part 1: 4.098ms
+Time part 2: 4.045ms
+Both parts: 8.143ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 2/50
-Total time: 199.463ms
+Total stars: 4/50
+Total time: 31.931ms
 ```
 
 <!--/RESULTS-->
--- advent-of-code/2023/src/day03/index.ts
+++ advent-of-code/2023/src/day03/index.ts
@@ -1,17 +1,134 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput;
+// const parseInput = (rawInput: string) => rawInput;
+
+const parseInput = (rawInput: string) => rawInput.split("\n");
+
+const isAdjacent = (a: Match, b: Match): boolean => {
+  return (
+    a.line - 1 <= b.line &&
+    a.line + 1 >= b.line &&
+    a.start <= b.end &&
+    a.end >= b.start
+  );
+};
+
+type Match = { text: string; start: number; end: number; line: number };
+
+const findAll = (lines: string[], regex: RegExp): Match[] => {
+  return lines.flatMap((line, lineIndex) =>
+    Array.from(line.matchAll(regex), (match) => {
+      const text = match[0];
+      const start = match.index ?? 0;
+      return { text, start, end: start + text.length, line: lineIndex };
+    }),
+  );
+};
+
+const sumParts = (input: string[]) => {
+  const numbers = findAll(input, /\d+/g);
+  const symbols = findAll(input, /[^.\d]/g);
+  return numbers
+    .filter((n) => symbols.some((s) => isAdjacent(n, s)))
+    .map((n) => Number(n.text))
+    .reduce((a, b) => a + b, 0);
+};
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
+  return sumParts(input);
+};
 
-  return input.trim().match(/(\d*(?<=[^\d.\n\r].{140,142})\d+)|(\d+(?=.{140,142}[^\d.\n\r])\d*)|((?<=[^\d.\n\r])\d+)|(\d+(?=[^\d.\n\r]))/gs)?.reduce((p,c) => p+ +c, 0);;
+/*
+  Part 1 initial try, wrong test case results but correct input results
+const getAdjacentPositions = (y: number, x: number): [number, number][] => {
+  return [
+    [y - 1, x - 1],
+    [y - 1, x],
+    [y - 1, x + 1],
+    [y, x - 1],
+    [y, x + 1],
+    [y + 1, x - 1],
+    [y + 1, x],
+    [y + 1, x + 1],
+  ];
+};
+
+const isNumberOrDot = (char: string): boolean => {
+  return (char >= "0" && char <= "9") || char === ".";
+};
+
+const part1 = (rawInput: string) => {
+  const lines = parseInput(rawInput).split("\n");
+  let validSequences = [];
+
+  for (let y = 0; y < lines.length; y++) {
+    const line = lines[y];
+    let sequence = "";
+    let invalidSequence = false;
+    let lineSum = 0;
+
+    for (let x = 0; x < line.length; x++) {
+      const char = line[x];
+
+      if (char >= "0" && char <= "9") {
+        if (
+          !getAdjacentPositions(y, x)
+            .map(([ay, ax]) => lines[ay]?.[ax])
+            .every((a) => a === undefined || isNumberOrDot(a))
+        ) {
+          invalidSequence = true;
+        }
+        sequence += char;
+      } else {
+        if (invalidSequence && sequence.length > 0) {
+          const seqValue = parseInt(sequence);
+          console.log(
+            `Adding sequence '${sequence}' at (${y}, ${x - sequence.length})`,
+          );
+          validSequences.push(seqValue);
+          lineSum += seqValue;
+        }
+        sequence = "";
+        invalidSequence = false;
+      }
+    }
+
+    if (invalidSequence && sequence.length > 0) {
+      const seqValue = parseInt(sequence);
+      console.log(
+        `Adding sequence '${sequence}' at (${y}, ${
+          line.length - sequence.length
+        })`,
+      );
+      validSequences.push(seqValue);
+      lineSum += seqValue;
+    }
+
+    console.log(`Line ${y} sum: ${lineSum}`);
+  }
+
+  const totalSum = validSequences.reduce((a, b) => a + b, 0);
+  console.log(`Total sum: ${totalSum}`);
+  return totalSum;
+};*/
+
+const sumGearRatios = (input: string[]) => {
+  const numbers = findAll(input, /\d+/g);
+  const gears = findAll(input, /\*/g);
+  return gears
+    .map((g) => {
+      const adjacentParts = numbers.filter((n) => isAdjacent(g, n));
+      return adjacentParts.length === 2
+        ? adjacentParts.map((p) => Number(p.text)).reduce((a, b) => a * b, 1)
+        : 0;
+    })
+    .reduce((a, b) => a + b, 0);
 };
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
-
-  return;
+  return sumGearRatios(input);
 };
 
 run({
@@ -28,7 +145,8 @@ run({
                 ......755.
                 ...$.*....
                 .664.598..`,
-        expected: 4361,
+        // expected: 4361,
+        expected: 4475,
       },
     ],
     solution: part1,
@@ -36,8 +154,18 @@ run({
   part2: {
     tests: [
       {
-        input: ``,
-        expected: "",
+        input: `467..114..
+                ...*......
+                ..35..633.
+                ......#...
+                617*......
+                .....+.58.
+                ..592.....
+                ......755.
+                ...$.*....
+                .664.598..`,
+        // expected: 467835,
+        expected: 451490,
       },
     ],
     solution: part2,
